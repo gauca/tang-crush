0d5539c23b060f3707e1cb58c10c82d4718a0ea8
gauca <85266978+gauca@users.noreply.github.com>
2021-06-03 09:40:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Thu à!',
+    introDesc: `Nhớ trả lời thật lòng nha mi :V`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Phải chăng bạn đã muốn khao mình một nồi lẩu sau dịch 😙',
+    desc: 'Phải chăng bạn đã muỗn bonus thêm ly trà sữa :V ',
+    btnYes: 'Vẫn cứ là chuẩn không cần chỉnh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    question:'Uầy bạn tốt ghê sao tự nhiên tốt với mình thế <3',
+    btnReply: 'Gửi cho mình <3',
+    reply: 'chịu thì chịu mà không chịu thì cũng cũng phả chịu :VVVV',
+    mess: 'mình biết mà 🥰. bạn là nhất 😘😘',
+    messDesc: 'qua dịch phải inbox lên kèo ngay và luôn nhé :v.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100029065882593' 
 }
